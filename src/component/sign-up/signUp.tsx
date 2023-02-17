06751aec0580e165d7a19b3bcfbc140e76a8df75
Betty Nguyen <bettyanhn@gmail.com>
2023-02-17 00:17:20 -0800

    WDGCA-3 Add style to sign up form

--- src/component/sign-up/signUp.tsx
+++ src/component/sign-up/signUp.tsx
@@ -1,22 +1,40 @@
 import React from "react";
 import Button from "../common/Button";
 import strings from "../../config/strings";
+import styles from "./signUp.module.scss";
+import classNames from "classnames";
 
 const SignUp = () => {
   return (
     <React.Fragment>
-      <div>
+      <div
+        className={classNames(
+          styles.signUp,
+          "py-3",
+          "d-flex",
+          "flex-column",
+          "align-items-center"
+        )}
+      >
         <h1>{strings.signUp.title}</h1>
         <form>
           <div>
             <input type="text" name="firstName" placeholder="First Name" />
+          </div>
+          <div>
             <input type="text" name="lastName" placeholder="Last Name" />
+          </div>
+          <div>
             <input
               type="text"
               name="emailAddress"
               placeholder="Email Address"
             />
+          </div>
+          <div>
             <input type="text" name="password" placeholder="Password" />
+          </div>
+          <div>
             <input
               type="text"
               name="verifyPassword"
