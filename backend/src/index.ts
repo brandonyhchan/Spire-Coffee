7c2b43b27f11a6f3562c2a2e3fc38a4d1b134b52
Brandon Chan <brandonchan45@gmail.com>
2023-02-22 20:53:11 -0800

    WDGCA-3 continue implementing backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,42 +1,35 @@
 import { PrismaClient } from "@prisma/client";
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from "apollo-server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import fs = require("fs");
 import path = require("path");
 
-import Query from "";
-
 const prisma = new PrismaClient();
 
-const server = new ApolloServer({
-  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
-  //   resolvers,
-  context: ({ req }) => {
-    return {
-      ...req,
-      prisma,
-    };
+const resolvers = {
+  Query: {
+    allUsers: () => {
+      return prisma.user.findMany();
+    },
   },
-});
+};
+console.log(__dirname);
 
-const url = async () => {
-  startStandaloneServer(server, {
-    listen: { port: 4000 },
+const runServer = () => {
+  const server = new ApolloServer({
+    typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+    resolvers,
+    context: () => {
+      return {
+        prisma,
+      };
+    },
+  });
+
+  const port = 4000;
+  server.listen(port, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}`);
   });
 };
 
-console.log(`Server ready at: ${url}`);
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+runServer();
