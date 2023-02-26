f3a52296d49ca450a6f91aebae0bd3af49c1d884
Izabelle Datayan <i_datayan@yahoo.com>
2023-02-25 20:17:34 -0800

    WDGCA-7 added the global style sheet and start on footerlink component

--- src/component/common/NavbarAndFooter/Footer.tsx
+++ src/component/common/NavbarAndFooter/Footer.tsx
@@ -3,12 +3,15 @@ import React, { MouseEventHandler } from "react";
 import classNames from "classnames";
 import "./Footer.scss";
 
+
 type FooterPropsType = {
   className?: string;
 };
 
 const footer = ({ className }: FooterPropsType) => (
-  <div className="footer">footer</div>
+  <div className="footer">
+    <p id="copyright">Copyright © 2023 SpireTech, Inc</p>
+  </div>
 );
 
 export default footer;
