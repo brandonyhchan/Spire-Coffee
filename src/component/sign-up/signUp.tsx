6d71ef1477c30495b224ab1a3b2fb0ccb27cd41a
Betty Nguyen <bettyanhn@gmail.com>
2023-03-07 23:27:38 -0800

    WDGCA-3 Add form input validation to sign up form

--- src/component/sign-up/signUp.tsx
+++ src/component/sign-up/signUp.tsx
@@ -1,54 +1,110 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
 import Button from "../common/Button";
 import strings from "../../config/strings";
 import classNames from "classnames";
 import styles from "./signUp.module.scss";
 
+type SignUpFormData = {
+  username: string;
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  password: string;
+  verifyPassword: string;
+};
+
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormData>();
+  
+  const onSubmit = (data: SignUpFormData) => {
+    console.log(data);
+  };
+
   return (
     <React.Fragment>
       <Helmet title={strings.signUp.helmet} />
       <h1>{strings.signUp.title}</h1>
       <div className={classNames(styles.signUpContainer)}>
         <div className={classNames(styles.signUp)}>
-          <form className={classNames(styles.signUpForm)}>
-            <input
-              type="text"
-              name="username"
-              placeholder={strings.signUp.usernameLabel}
-            />
-            <input
-              type="text"
-              name="firstName"
-              placeholder={strings.signUp.firstNameLabel}
-            />
-            <input
-              type="text"
-              name="lastName"
-              placeholder={strings.signUp.lastNameLabel}
-            />
-            <input
-              type="text"
-              name="emailAddress"
-              placeholder={strings.signUp.emailLabel}
-            />
-            <input
-              type="text"
-              name="password"
-              placeholder={strings.signUp.passwordLabel}
-            />
-            <input
-              type="text"
-              name="verifyPassword"
-              placeholder={strings.signUp.verifyPasswordLabel}
-            />
-          </form>
-          <Button
+          <form 
+            className={classNames(styles.signUpForm)}
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+            >
+            <div className={classNames(styles.formItem)}>
+              <input
+                type="text"
+                placeholder={strings.signUp.usernameLabel}
+                required
+                {...register("username", { required: true })}
+              />
+              <label>{strings.signUp.usernameLabel}</label>
+              {errors.username && <span>Username is required.</span>}
+            </div>
+            <div className={classNames(styles.formItem)}>
+              <input
+                type="text"
+                placeholder={strings.signUp.firstNameLabel}
+                required
+                {...register("firstName", { required: true })}
+              />
+              <label>{strings.signUp.firstNameLabel}</label>
+              {errors.firstName && <span>First name is required.</span>}
+            </div>
+            <div className={classNames(styles.formItem)}>
+              <input
+                type="text"
+                placeholder={strings.signUp.lastNameLabel}
+                required
+                {...register("lastName", { required: true })}
+              />
+              <label>{strings.signUp.lastNameLabel}</label>
+              {errors.lastName && <span>Last name is required.</span>}
+            </div>
+            <div className={classNames(styles.formItem)}>
+              <input
+                type="text"
+                placeholder={strings.signUp.emailLabel}
+                required
+                {...register("emailAddress", { required: true })}
+              />
+              <label>{strings.signUp.emailLabel}</label>
+              {errors.emailAddress && <span>Email address is required.</span>}
+            </div>
+            <div className={classNames(styles.formItem)}>
+              <input
+                type="text"
+                placeholder={strings.signUp.passwordLabel}
+                required
+                {...register("password", { required: true })}
+              />
+              <label>{strings.signUp.passwordLabel}</label>
+              {errors.password && <span>Password is required.</span>}
+            </div>
+            <div className={classNames(styles.formItem)}>
+              <input
+                type="text"
+                placeholder={strings.signUp.verifyPasswordLabel}
+                required
+                {...register("verifyPassword", { required: true })}
+              />
+              <label>{strings.signUp.verifyPasswordLabel}</label>
+              {errors.verifyPassword && (
+                <span>Please re-enter your password.</span>
+              )}
+            </div>
+            <Button
             buttonType="submit"
             className="signUpButton"
             text={strings.signUp.buttonText}
           />
+          </form>
         </div>
       </div>
     </React.Fragment>
