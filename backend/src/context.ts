b6afab2cecaf146106a592a0f95b63bf43113cc2
Brandon Chan <brandonchan45@gmail.com>
2023-03-08 21:24:49 -0800

    WDGCA-3 added submittable strings and frontend submit logic to signup page

--- backend/src/context.ts
+++ backend/src/context.ts
@@ -3,18 +3,17 @@ import { getUserId } from "./utils.js";
 import { Request } from "express";
 import { JwtPayload } from "jsonwebtoken";
 
-
 export const prisma = new PrismaClient();
 
 export interface Context {
-    prisma: PrismaClient;
-    userId: string | JwtPayload | null;
+  prisma: PrismaClient;
+  userId: string | JwtPayload | null;
 }
 
 export const context = ({ req }: { req: Request }): Context => {
-    return {
-        ...req,
-        prisma,
-        userId: req && req.headers.authorization ? getUserId(req) : null,
-    };
+  return {
+    ...req,
+    prisma,
+    userId: req && req.headers.authorization ? getUserId(req) : null,
+  };
 };
