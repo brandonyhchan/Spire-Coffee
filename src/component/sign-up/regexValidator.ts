358e36aafd684a8d2742c9d8ff272f5eb5e3c2c4
Betty Nguyen <bettyanhn@gmail.com>
2023-03-20 00:10:43 -0700

    WDGCA-3 Change regex for email validator

--- src/component/sign-up/regexValidator.ts
+++ src/component/sign-up/regexValidator.ts
@@ -1,12 +1,11 @@
 export default class RegexValidator {
-  validUsername = new RegExp("^[a-zA-Z0-9_]{5,15}$"); // lower upper number underscore min 5 max 15
+  validUsername = new RegExp("^[a-zA-Z0-9_]{5,15}$");
 
-  validFirstName = new RegExp("^[A-Za-z]+$"); //include spaces in names, hyphens periods
-  validLastName = new RegExp("^[A-Za-z]+$"); //include spaces, hyphens periods
+  validFirstName = new RegExp("^[A-Za-z]+(?:[A-Za-z .-]+)+$");
+  validLastName = new RegExp("^[A-Za-z]+(?:[A-Za-z .-]+)+$");
 
   validPassword = new RegExp(
-    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])" // ^starts with at least 1 lowercase at least 1 uppercase at least 1 number at least one of the given special characters minimum length 8 max 20 $end string
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])"
   );
-
-  validEmail = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}"); //cannot end in trailing period
+  validEmail = new RegExp("^[a-zA-Z0-9]+[a-zA-Z0-9-.]*@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
 }
