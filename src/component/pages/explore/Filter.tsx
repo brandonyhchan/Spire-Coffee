0bf83d83a2d611d05f17bbad163ba5fb20df9eb2
Betty Nguyen <bettyanhn@gmail.com>
2023-03-31 21:32:45 -0700

    WDGCA-15 Incomplete implementation of filter

---
+++ src/component/pages/explore/Filter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./filter.module.scss";
+
+const Filter = () => <div className={classNames(styles.container)}>hello</div>;
+
+export default Filter;
--- src/component/pages/explore/explore.tsx
+++ src/component/pages/explore/explore.tsx
@@ -4,6 +4,7 @@ import { Helmet } from "react-helmet";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import NavBar from "component/common/NavbarAndFooter/NavBar";
 import Footer from "component/common/NavbarAndFooter/WebFooter";
+import Filter from "./Filter";
 import MobileFooter from "component/common/NavbarAndFooter/MobileFooter";
 import classNames from "classnames";
 import { CafeData } from "./mockCafeData";
@@ -39,43 +40,50 @@ const Explore = () => {
         <div className={classNames(styles.explore)}>
           {/*Remove the title after*/}
           <p>{strings.explore.title}</p>
-          <div className={classNames(styles.searchBarContainer)}>
-            <form className={classNames(styles.searchBar)}>
-              <SearchRoundedIcon
-                className={classNames(styles.searchBarIcon)}
-              ></SearchRoundedIcon>
-              <input
-                type="text"
-                placeholder="Search..."
-                name="search"
-                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                  setQuery(event.target.value)
-                }
-                onFocus={() => setShowCloseButton(true)}
-                value={query}
-              />
-              {!showCloseButton ? null : (
-                <div className={classNames(styles.clearButtonContainer)}>
-                  <CloseRoundedIcon
-                    className={classNames(styles.clearButton)}
-                    // onClick={() => setQuery("")}
-                    onClick={handleClick}
-                  ></CloseRoundedIcon>
-                </div>
-              )}
-            </form>
-          </div>
-          {CafeData.filter((cafe) => {
-            if (query === "") {
-              return cafe;
-            } else if (cafe.name.toLowerCase().includes(query.toLowerCase())) {
-              return cafe;
-            }
-          }).map((cafe, index) => (
-            <div key={index}>
-              <p>{cafe.name}</p>
+          <div className={classNames(styles.exploreContainer)}>
+            <div className={classNames(styles.filterContainer)}>
+              <Filter></Filter>
             </div>
-          ))}
+            <div className={classNames(styles.searchBarContainer)}>
+              <form className={classNames(styles.searchBar)}>
+                <SearchRoundedIcon
+                  className={classNames(styles.searchBarIcon)}
+                ></SearchRoundedIcon>
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  name="search"
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    setQuery(event.target.value)
+                  }
+                  onFocus={() => setShowCloseButton(true)}
+                  value={query}
+                />
+                {!showCloseButton ? null : (
+                  <div className={classNames(styles.clearButtonContainer)}>
+                    <CloseRoundedIcon
+                      className={classNames(styles.clearButton)}
+                      // onClick={() => setQuery("")}
+                      onClick={handleClick}
+                    ></CloseRoundedIcon>
+                  </div>
+                )}
+              </form>
+              {CafeData.filter((cafe) => {
+                if (query === "") {
+                  return cafe;
+                } else if (
+                  cafe.name.toLowerCase().includes(query.toLowerCase())
+                ) {
+                  return cafe;
+                }
+              }).map((cafe, index) => (
+                <div key={index}>
+                  <p>{cafe.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         <Footer />
         <MobileFooter />
