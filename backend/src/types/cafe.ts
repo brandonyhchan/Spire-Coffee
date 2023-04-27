e08680f351a2b59c2dc943e21485a439a9edbb14
Brandon Chan <brandonchan45@gmail.com>
2023-04-26 21:34:32 -0700

    WDGCA-15 addition of function to calculate distance from 2 lat & long, DOES NOT BREAK ANY FEATURES ONLY TO BRANCH FROM

---
+++ backend/src/types/cafe.ts
@@ -0,0 +1,22 @@
+export type Cafe = {
+  id: number;
+  name: string;
+  stringId: string;
+  street: string;
+  city: string;
+  province: string;
+  profilePhotoURL: string;
+  busyness: string;
+  noisiness: string;
+  price: string;
+  location: Location;
+};
+
+export interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+export type distance<T> = T & {
+  distance: number;
+};
--- src/support/graphqlServerApi.ts
+++ src/support/graphqlServerApi.ts
@@ -63,6 +63,7 @@ export const returnAllCafeQuery = gql`
       busyness
       noisiness
       price
+      distance
     }
   }
 `;
