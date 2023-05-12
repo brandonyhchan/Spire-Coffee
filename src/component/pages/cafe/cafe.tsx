50cbbd913667ae047378990e31d5d1fe0ea29265
Betty Nguyen <bettyanhn@gmail.com>
2023-05-11 21:04:16 -0700

    WDGCA-17 Fix styles for cafe page after change to using grid

--- src/component/pages/cafe/cafe.tsx
+++ src/component/pages/cafe/cafe.tsx
@@ -110,83 +110,85 @@ const CafePage = () => {
               <ImageCarousel />
             </div>
             <div className={classNames(styles.infoContainer)}>
-              <div className={classNames(styles.cafeDetails)}>
-                <p>{cafe?.street}</p>
-                <p>
-                  {cafe?.city}
-                  {strings.global.comma}
-                  {strings.global.space}
-                  {cafe?.province}
-                  {strings.global.space}
-                  {cafe?.postalCode}
-                </p>
-                <div className={classNames(styles.labelContainer)}>
-                  <AccessTimeRoundedIcon />
-                  <label>
-                    <Dropdown text={strings.cafe.businessHours}>
-                      {businessHours.map((hours, index) => (
-                        <div
-                          key={index}
-                          className={classNames(styles.dropdownItem)}
-                        >
-                          <div className={classNames(styles.cafeDays)}>
-                            <p>{hours.weekday}</p>
+              <div className={classNames(styles.cafeInfo)}>
+                <div className={classNames(styles.cafeDetails)}>
+                  <p>{cafe?.street}</p>
+                  <p>
+                    {cafe?.city}
+                    {strings.global.comma}
+                    {strings.global.space}
+                    {cafe?.province}
+                    {strings.global.space}
+                    {cafe?.postalCode}
+                  </p>
+                  <div className={classNames(styles.labelContainer)}>
+                    <AccessTimeRoundedIcon />
+                    <label>
+                      <Dropdown text={strings.cafe.businessHours}>
+                        {businessHours.map((hours, index) => (
+                          <div
+                            key={index}
+                            className={classNames(styles.dropdownItem)}
+                          >
+                            <div className={classNames(styles.cafeDays)}>
+                              <p>{hours.weekday}</p>
+                            </div>
+                            <div className={classNames(styles.cafeHours)}>
+                              <p>{hours.hours}</p>
+                            </div>
                           </div>
-                          <div className={classNames(styles.cafeHours)}>
-                            <p>{hours.hours}</p>
-                          </div>
-                        </div>
-                      ))}
-                    </Dropdown>
-                  </label>
+                        ))}
+                      </Dropdown>
+                    </label>
+                  </div>
+                  <div className={classNames(styles.labelContainer)}>
+                    <LocalPhoneRoundedIcon />
+                    <label>{renderPhoneNumber()}</label>
+                  </div>
+                  {/* are we linking this to the external website */}
+                  <div className={classNames(styles.labelContainer)}>
+                    <LanguageRoundedIcon />
+                    {renderWebsite()}
+                  </div>
+                  {/* these might be turned into sliders after */}
+                  <div className={classNames(styles.labelContainer)}>
+                    {renderBusyIcon(cafe?.busyness)}
+                    <label>
+                      {strings.cafe.busynessLabel}
+                      {strings.global.semiColon}
+                      {strings.global.space}
+                      {renderBusyText(cafe?.busyness)}
+                    </label>
+                  </div>
+                  <div className={classNames(styles.labelContainer)}>
+                    {renderNoiseIcon(cafe?.noisiness)}
+                    <label>
+                      {strings.cafe.noisinessLabel}
+                      {strings.global.semiColon}
+                      {strings.global.space}
+                      {renderNoiseText(cafe?.noisiness)}
+                    </label>
+                  </div>
+                  <div className={classNames(styles.labelContainer)}>
+                    {renderPrice()}
+                    <label>
+                      {strings.cafe.priceLabel}
+                      {strings.global.semiColon}
+                      {strings.global.space}
+                      {renderPriceText(cafe?.price)}
+                    </label>
+                  </div>
                 </div>
-                <div className={classNames(styles.labelContainer)}>
-                  <LocalPhoneRoundedIcon />
-                  <label>{renderPhoneNumber()}</label>
+                <div
+                  onClick={handleFavouriteButton}
+                  className={classNames(styles.favouriteButton)}
+                >
+                  {!favourite ? (
+                    <FavoriteBorderRoundedIcon />
+                  ) : (
+                    <FavoriteRoundedIcon />
+                  )}
                 </div>
-                {/* are we linking this to the external website */}
-                <div className={classNames(styles.labelContainer)}>
-                  <LanguageRoundedIcon />
-                  {renderWebsite()}
-                </div>
-                {/* these might be turned into sliders after */}
-                <div className={classNames(styles.labelContainer)}>
-                  {renderBusyIcon(cafe?.busyness)}
-                  <label>
-                    {strings.cafe.busynessLabel}
-                    {strings.global.semiColon}
-                    {strings.global.space}
-                    {renderBusyText(cafe?.busyness)}
-                  </label>
-                </div>
-                <div className={classNames(styles.labelContainer)}>
-                  {renderNoiseIcon(cafe?.noisiness)}
-                  <label>
-                    {strings.cafe.noisinessLabel}
-                    {strings.global.semiColon}
-                    {strings.global.space}
-                    {renderNoiseText(cafe?.noisiness)}
-                  </label>
-                </div>
-                <div className={classNames(styles.labelContainer)}>
-                  {renderPrice()}
-                  <label>
-                    {strings.cafe.priceLabel}
-                    {strings.global.semiColon}
-                    {strings.global.space}
-                    {renderPriceText(cafe?.price)}
-                  </label>
-                </div>
-              </div>
-              <div
-                onClick={handleFavouriteButton}
-                className={classNames(styles.favouriteButton)}
-              >
-                {!favourite ? (
-                  <FavoriteBorderRoundedIcon />
-                ) : (
-                  <FavoriteRoundedIcon />
-                )}
               </div>
             </div>
             <div className={classNames(styles.reportContainer)}>
