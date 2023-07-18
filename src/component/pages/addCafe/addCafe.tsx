3c53effa65611b7184b07f1b22474c2030678758
Betty Nguyen <bettyanhn@gmail.com>
2023-07-17 20:35:37 -0700

    WDGCA-69 Add form component

--- src/component/pages/addCafe/addCafe.tsx
+++ src/component/pages/addCafe/addCafe.tsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import classNames from "classnames";
 import NavBar from "component/common/NavbarAndFooter/NavBar";
-
+import Form from "component/common/Form/Form";
+import FormItem from "component/common/Form/FormItem";
 import styles from "./addCafe.module.scss";
 import strings from "config/strings";
 
@@ -18,13 +19,36 @@ const AddCafe = () => {
     }
   }, [navigate, token]);
 
+  const handleAddCafe = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.currentTarget.value;
+    // setUserInfo({
+    //   ...userInfo,
+    //   [name]: value,
+    // });
+  };
+
   return (
     <React.Fragment>
       <Helmet title={strings.addCafe.helmet} />
       <div className={classNames(styles.container)}>
         <NavBar />
         <div className={classNames(styles.addCafe)}>
-          <p>{strings.addCafe.title}</p>
+          <Form
+            className={classNames(styles.addCafeForm)}
+            handleForm={handleAddCafe}
+          >
+            <FormItem type={"text"} handleChange={handleChange} name="name" />
+            <FormItem
+              type={"text"}
+              handleChange={handleChange}
+              name="address"
+            />
+          </Form>
         </div>
       </div>
     </React.Fragment>
